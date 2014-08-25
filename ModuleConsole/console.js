2b16cf423fdedc9e7278a49a6335b9054cf4bc1f
ivpopov <iv.popov@estp.ru>
2014-08-25 09:57:43 +0400

    Модуль Console

---
+++ ModuleConsole/console.js
@@ -0,0 +1,10 @@
+/**
+ * В модуле console всего два метода
+ * console.info = console.log - выводит в стандартный поток вывода
+ * console.error = console.warn - выводит в лог ошибок
+ * Запуск
+ * node console.js
+ * node console.js 1>ok 2>err
+ */
+console.log("Log");
+console.error("Error");
